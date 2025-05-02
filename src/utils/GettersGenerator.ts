e51399442f02ce0f83984bbbed263fe485ba44f5
aguizzo <andres.guizzo@gmail.com>
2025-05-02 20:38:52 +0200

    feat: add createGettersWithLogging function for logging access to object properties

--- src/utils/GettersGenerator.ts
+++ src/utils/GettersGenerator.ts
@@ -15,3 +15,17 @@ export function createGetters<T>(obj: T): Getters<T> {
     
     return result;
 }
+
+export function createGettersWithLogging<T>(obj: T): Getters<T> {
+    const result = {} as Getters<T>;
+    
+    for (const key in obj) {
+        const getterName = `get${capitalize(key)}` as keyof Getters<T>;
+        result[getterName] = (() => {
+            console.info(`Accessed ${key}: ${obj[key]}`);
+            return obj[key];
+        }) as Getters<T>[keyof Getters<T>];
+    }
+    
+    return result;
+}
